bfab6e843d13d362d2cb7742037a1f80974a34b4
MikeVey42 <steelege07@gmail.com>
2024-01-24 09:39:09 -0500

    Convergent Shot Calculation

--- src/main/java/frc/robot/ShooterMath.java
+++ src/main/java/frc/robot/ShooterMath.java
@@ -5,27 +5,50 @@ package frc.robot;
  */
 public final class ShooterMath {
     
-    public class ShotDetails {
-        private final double velocity, angle;
-        public ShotDetails(double velocity, double angle) {
-            this.velocity = velocity;
-            this.angle = angle;
+    public static class ShotDetails {
+        private final double xVelocity, yVelocity;
+        public ShotDetails(double xVelocity, double yVelocity) {
+            this.xVelocity = xVelocity;
+            this.yVelocity = yVelocity;
         }
 
+        /**
+         * @return the horizontal velocity of the note towards the speaker (in meters)
+         */
+        public double getXVelocity() {
+          return xVelocity;
+        }
+
+        /**
+         * @return the vertical velocity of the note (in meters)
+         */
+        public double getYVelocity() {
+          return yVelocity;
+        }
+        
+        /**
+         * @return the total velocity of the note (in meters)
+         */
         public double getLaunchVelocity() {
-            return velocity;
+            return Math.hypot(xVelocity, yVelocity);
         }
 
+        /**
+         * @return the angle that the note is shot at relative to the ground (in degrees)
+         */
         public double getLaunchAngle() {
-            return angle;
+            return Math.toDegrees(Math.atan(yVelocity/xVelocity));
         }
 
+        /**
+         * @return the angle of the line, from the pivot to the high flywheel, to the ground. (in degrees)
+         */
         public double getShooterAngle() {
-            return shooterAngleToLaunchAngle(angle);
+            return shooterAngleToLaunchAngle(getLaunchAngle());
         }
 
         public double getFlywheelVelocity() {
-            return velocity;
+            return getLaunchVelocity();
         }
     }
 
@@ -66,33 +89,47 @@ public final class ShooterMath {
    * @return the angle the note will be launched at given that shooter angle (in degrees)
    */
   public static double shooterAngleToLaunchAngle(double shooterAngle) {
-    return Constants.kShooter.LAUNCH_ANGLE_TO_SHOOTER_ANGLE + shooterAngle;
+    return Constants.kShooter.LAUNCH_ANGLE_TO_SHOOTER_ANGLE - shooterAngle;
   }
 
-  public static double getLaunchAngle(double distance, double height) {
+  /**
+   * Calculates the details of an optimal shot based on the height and distance of a shooter
+   * @param distance the horizontal distance of the shooter from the speaker (in meters)
+   * @param height the height of the shooter off the ground (in meters)
+   * @return details of the optimal shot
+   */
+  public static ShotDetails getShot(double distance, double height) {
     // see https://www.desmos.com/calculator/sfjrd3ja6f
-    double yVelocity = Math.sqrt(((80.5/12) - height) * 32 * 2); // y velocity of the note (ft/s) 
-    double xVelocity = 32 * distance / yVelocity; // x velocity of the note (ft/s)
-    double launchAngle = Math.atan(yVelocity / xVelocity); // Angle to launch the note at, in radians
-    return launchAngle;
+    double yVelocity = Math.sqrt(((80.5/12) - (3.28084 * height)) * 32 * 2); // y velocity of the note (ft/s) 
+    double xVelocity = 32 * (3.28084 * distance) / yVelocity; // x velocity of the note (ft/s)
+    // Convert back to meters
+    yVelocity *= 0.3048;
+    xVelocity *= 0.3048;
+
+    return new ShotDetails(xVelocity, yVelocity);
   }
 
+  /**
+   * Calculates the details of an optimal shot based on only the distance of the robot from the speaker
+   * @param robotDistance
+   * @return
+   */
   public static ShotDetails getShot(double robotDistance) {
     double distance = robotDistance + Constants.kShooter.PIVOT_X; // Initial guess for distance from the shooter to the speaker
     double height = Constants.kShooter.PIVOT_Y; // Initial guess for height of the shooter from the ground
 
 
     for (int i = 0; i < 2; i++) {
-        // Get initial guess for launch angle of the note
-        double launchAngle = getLaunchAngle(distance, height);
-        // Find the corresponding angle for the shooter
-        double shooterAngle = launchAngleToShooterAngle(launchAngle);
+        // Calculate the shooter angle according to the current guess for shooter position
+        double shooterAngle = getShot(distance, height).getShooterAngle();
         // Update the shooter distance and height based on the new angle
         distance = getShooterDistance(shooterAngle, robotDistance);
         height = getShooterHeight(shooterAngle);
 
         // Repeat as many times as nessecary to get an accurate shot
     }
+
+    return getShot(distance, height);
 
   }
 }
