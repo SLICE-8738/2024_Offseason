f417e8e071ff497b55d379e4f505901a63ec3e15
Yug-Sarin <321sarinyug@gmail.com>
2024-03-20 14:28:20 -0400

    Removed the aim motor from ShooterTestCommand

--- src/main/java/frc/robot/testing/routines/ShooterTestCommand.java
+++ src/main/java/frc/robot/testing/routines/ShooterTestCommand.java
@@ -15,14 +15,14 @@ import frc.robot.subsystems.Shooter;
 public class ShooterTestCommand extends Command {
   /** Creates a new ShooterTestCommand. */
   private Shooter shooter;
-  private double[] averageCurrent = {0,0,0};
-  private double[] maxCurrent = {0,0,0};
-  private double[] averageSpeed = {0,0,0};
+  private double[] averageCurrent = {0,0};
+  private double[] maxCurrent = {0,0};
+  private double[] averageSpeed = {0,0};
   private int executes = 0;
   private final Timer timer;
 
   private ShuffleboardTab testRoutine;
-  private SimpleWidget flywheelTopCurrenWidget, flywheelBottomCurrentWidget, aimMotorCurrentWidget, flywheelTopVelocityWidget, flywheelBottomVelocityWidget, aimMotorVelocityWidget;
+  private SimpleWidget flywheelTopCurrenWidget, flywheelBottomCurrentWidget, flywheelTopVelocityWidget, flywheelBottomVelocityWidget;
   
   public ShooterTestCommand(Shooter shooter) {
   
@@ -34,10 +34,8 @@ public class ShooterTestCommand extends Command {
   testRoutine = Shuffleboard.getTab("Test Routine");
   flywheelTopCurrenWidget = testRoutine.add("Top Flywheel Current", 0);
   flywheelBottomCurrentWidget = testRoutine.add("Bottom Flywheel Current", 0);
-  aimMotorCurrentWidget = testRoutine.add("Aim Motor Current", 0);
   flywheelTopVelocityWidget = testRoutine.add("Top Flywheel Velocity", 0);
   flywheelBottomVelocityWidget = testRoutine.add("Bottom flywheel; Velocity", 0);
-  aimMotorVelocityWidget = testRoutine.add("Aim Motor Velocity", 0);
 
   }
 
@@ -46,7 +44,6 @@ public class ShooterTestCommand extends Command {
   public void initialize() {
     shooter.flywheelTop.set(1);
     shooter.flywheelBottom.set(1);
-    shooter.aimMotor.set(1);
     timer.restart();
   }
 
@@ -57,12 +54,10 @@ public class ShooterTestCommand extends Command {
 
     averageCurrent[0] += shooter.flywheelTop.getOutputCurrent();
     averageCurrent[1] += shooter.flywheelBottom.getOutputCurrent();
-    averageCurrent[2] += shooter.aimMotor.getOutputCurrent();
 
     // Outputting to Shuffleboard
     flywheelTopCurrenWidget.getEntry().setDouble(shooter.flywheelTop.getOutputCurrent());
     flywheelBottomCurrentWidget.getEntry().setDouble(shooter.flywheelBottom.getOutputCurrent());
-    aimMotorCurrentWidget.getEntry().setDouble(shooter.aimMotor.getOutputCurrent());
 
     if(shooter.flywheelTop.getOutputCurrent() > maxCurrent[0]){
       maxCurrent[0] = shooter.flywheelTop.getOutputCurrent();
@@ -70,18 +65,13 @@ public class ShooterTestCommand extends Command {
     if(shooter.flywheelBottom.getOutputCurrent() > maxCurrent[1]){
       maxCurrent[1] = shooter.flywheelBottom.getOutputCurrent();
     }
-    if(shooter.aimMotor.getOutputCurrent() > maxCurrent[2]){
-      maxCurrent[2] = shooter.aimMotor.getOutputCurrent();
-    }
 
     averageSpeed[0] += shooter.flyTopEncoder.getVelocity();
     averageSpeed[1] += shooter.flyBottomEncoder.getVelocity();
-    averageSpeed[2] += shooter.aimEncoder.getVelocity();
 
     // Outputting to Shuffleboard
     flywheelTopVelocityWidget.getEntry().setDouble(shooter.flyTopEncoder.getVelocity());
     flywheelBottomVelocityWidget.getEntry().setDouble(shooter.flyBottomEncoder.getVelocity());
-    aimMotorVelocityWidget.getEntry().setDouble(shooter.aimEncoder.getVelocity());
 
 
     if(timer.get() >= 2){
@@ -95,7 +85,6 @@ public class ShooterTestCommand extends Command {
   public void end(boolean interrupted) {
     shooter.flywheelTop.set(0);
     shooter.flywheelBottom.set(0);
-    shooter.aimMotor.set(0);
 
     for(int i = 0; i < 3; i++){
       averageCurrent[i] /= executes;
@@ -103,9 +92,9 @@ public class ShooterTestCommand extends Command {
     }
 
     System.out.println("Shooter Tests");
-    System.out.println(String.format("Average Current: \nTop Motor: %s \nBottom Motor: %s \nAim Motor: %s", averageCurrent[0], averageCurrent[1], averageCurrent[2]));
-    System.out.println(String.format("Max Current: \nTop Motor: %s \nBottom Motor: %s \nAim Motor: %s", maxCurrent[0], maxCurrent[1], maxCurrent[2]));
-    System.out.println(String.format("Average Speed: \nTop Motor: %s \nBottom Motor: %s \nAim Motor: %s", averageSpeed[0], averageSpeed[1], averageSpeed[2]));
+    System.out.println(String.format("Average Current: \nTop Motor: %s \nBottom Motor: %s", averageCurrent[0], averageCurrent[1]));
+    System.out.println(String.format("Max Current: \nTop Motor: %s \nBottom Motor: %s", maxCurrent[0], maxCurrent[1]));
+    System.out.println(String.format("Average Speed: \nTop Motor: %s \nBottom Motor: %s", averageSpeed[0], averageSpeed[1]));
   }
 
   // Returns true when the command should end.
